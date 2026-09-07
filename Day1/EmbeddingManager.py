eaea9c08a385e0ce69e1a908e1d92695f4eb94d6
nalla <nallaperumal@gmail.com>
2026-09-07 13:02:14 +0530

    converted text to embedding

--- Day1/EmbeddingManager.py
+++ Day1/EmbeddingManager.py
@@ -1,4 +1,5 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
 
 class EmbeddingManager:
 
@@ -12,3 +13,9 @@ class EmbeddingManager:
 
         chunk_list = [chunk for chunk in text_splitter.split_text(full_content)] 
         return chunk_list
+
+    def convert_txt_to_embed(self, chunks):
+        model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        embeddings  = model.encode(chunks)
+        for emb in embeddings:
+            print(f"len is....{len(emb)}.. sample {emb[:3]}")
--- Day1/rag_app.py
+++ Day1/rag_app.py
@@ -8,10 +8,11 @@ context_bun = fileMan.ReadFromFile()
 embedMan = EmbeddingManager()
 
 chunk_list = embedMan.GetChunks(context_bun)
+embedMan.convert_txt_to_embed(chunk_list)
 print(f"number of chunks is {len(chunk_list)}")
 
 # for each_chunk in chunk_list:
 #     print(each_chunk)
 #     print("===================================")
 
-print("\n============------==========\n".join(chunk_list))
+# print("\n============------==========\n".join(chunk_list))
